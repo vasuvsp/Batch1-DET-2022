aa6ea49802142b96760143fa411eb93f828889e8
Vasu.Patil <vasuspatil555@gmail.com>
2022-08-26 15:21:12 +0530

    Add new messages to console

---
+++ Batch1-DET-2022/SalaryInfo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class SalaryInfo
+    {
+        int EmpNo;
+        string  Ename;
+        string Address;
+        int pin_code;
+        int Phone_no;
+        int Grass_salary;
+        int pf;
+        public SalaryInfo(int EmpNo, string Ename, string Address, int pin_code, int Phone_no, int Grass_salary)
+        {
+            this.EmpNo = EmpNo; this.Ename = Ename; this.Address = Address; this.pin_code = pin_code;
+            this.Phone_no = Phone_no; this.Grass_salary = Grass_salary;
+        }
+        public int GetNetSalary()
+        {
+            pf = 12 * Grass_salary / 100;
+            int netsal = Grass_salary - pf;
+            return netsal;
+        }
+        public char GetEmpGrade()
+        {
+            if (GetNetSalary() > 10000)
+            {
+                return 'a';
+            }
+            else if (GetNetSalary() > 5000)
+            {
+                return 'B';
+            }
+            else 
+                    return 'C';
+        }
+
+    }
+}
+
+
+    
---
+++ Batch1-DET-2022/TestSalaryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestSalaryInfo
+    {
+        public static void Main()
+        {
+            SalaryInfo e = new SalaryInfo(22809, "vasu", "Raichur", 584103, 859546, 30838);
+            Console.WriteLine($"Employee Net Salary {e.GetNetSalary()}");
+            Console.WriteLine($"Employee Net Salary {e.GetEmpGrade()}");
+
+        }
+    }
+}
