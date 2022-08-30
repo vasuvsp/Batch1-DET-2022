940107f5f6d5c3c274a1e6d763032a55d9607e86
Vasu.Patil <vasuspatil555@gmail.com>
2022-08-30 18:18:30 +0530

    Add new message to console

---
+++ Myclasslibrary/Emp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myclasslibrary
+{
+    public class Emp
+    {
+        int id;
+        string name;
+        string dept;
+        double sal;
+        //DateTime doj;
+
+        public DateTime doj { get; set; }
+
+        //constructor - is a function used to init object data
+        //has the same name as classname, no ret type
+        public Emp(int id, string name, DateTime doj)
+        {
+            this.id = id; this.name = name; this.doj = doj;
+        }
+        public Emp()
+        {
+            id = -1; name = "no name";
+        }
+        public Emp(int id, string name)
+        {
+            this.id = id; this.name = name;
+        }
+
+        //function written inside a class is known as method
+        public int GetYearsofExp()
+        {
+            return DateTime.Now.Year - doj.Year;
+        }
+
+        public virtual string ToString()   //only virtual method can be overridden
+        {
+            return $"Emp Id={id}, Name={name}, Experience={GetYearsofExp()} Years";
+        }
+    }
+}
---
+++ TestProject_MyLibrary/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Myclasslibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject_MyLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Emp e1 = new Emp(22825, "Manasa", new DateTime(2005, 8, 31));
+            DateTime expected = new DateTime(2005, 8, 31);
+
+            //Act
+            DateTime actual = e1.doj;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestYearsOfExp()
+        {
+            Emp e1 = new Emp(22825, "Manasa", new DateTime(2005, 8, 31));
+            int experience = 17;
+
+            int actual = e1.GetYearsofExp();
+            Assert.AreEqual(experience, actual);
+        }
+    }
+}
